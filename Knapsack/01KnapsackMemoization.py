a6ac5ecfc586af28f95d9eddcc31386a89a23629
Purushotam <purushotamsangroula@gmail.com>
2022-08-04 11:45:43 +0545

    knapsack with top down approach

--- Knapsack/01KnapsackMemoization.py
+++ Knapsack/01KnapsackMemoization.py
@@ -1,6 +1,6 @@
 
 def ks2(wt, val, w, n):
-    if n < 0 or w == 0:
+    if n == 0 or w == 0:
         return 0
     global dp
     try:
@@ -8,25 +8,28 @@ def ks2(wt, val, w, n):
             return dp[n][w]
     except:
         print("ex: ", n,w)
-    if wt[n] <= w:
+    if wt[n-1] <= w:
         dp[n][w] = max(
-            val[n] + ks2(wt, val, w-wt[n], n-1 ),
+            val[n-1] + ks2(wt, val, w-wt[n-1], n-1 ),
             ks2(wt, val, w, n - 1)
         )
-    elif wt[n] >= w:
+    elif wt[n-1] > w:
         dp[n][w] = ks2(wt, val, w, n - 1)
 
     return dp[n][w]
 
 
-val = [ 20, 5, 10, 40, 15, 25 ]
-wt = [ 1, 2, 3, 8, 7, 4 ]
-w = 10
+# val = [ 20, 5, 10, 40, 15, 25 ]
+# wt = [ 1, 2, 3, 8, 7, 4 ]
+# w = 10
 
-# w = 50
-# val = [60, 100, 120]
-# wt = [10, 20, 30]
-r, c = len(val) +1  , w +1
-dp = [[-1]*c]*r
+w = 50
+val = [60, 100, 120]
+wt = [10, 20, 30]
+n, w = len(val), w
+dp = [[-1 for _ in range(w+1)] for _ in range(n+1)]
 # print(dp)
-print(ks2(wt, val, w, len(val)-1))
+
+print(ks2(wt, val, w, n))
+
+print(dp)
--- Knapsack/01knapsackTDApproach.py
+++ Knapsack/01knapsackTDApproach.py
@@ -1,9 +1,7 @@
-
 def ksdp(td, wt, val, n, w):
-
     try:
-        for i in range(n):
-            for j in range(w):
+        for i in range(1, n + 1):
+            for j in range(1, w + 1):
                 if wt[i-1] <= j:
                     td[i][j] = max(
                         val[i-1] + td[i-1][j-wt[i-1]],
@@ -13,32 +11,21 @@ def ksdp(td, wt, val, n, w):
                     td[i][j] = td[i - 1][j]
     except:
         print("except: ", n,w)
-    for i in range(n):
+    for i in range(n+1):
         print(td[i])
-    # return td[n][w]
-
+    return td[n][w]
 
 
 val = [ 20, 5, 10, 40, 15, 25 ]
 wt = [ 1, 2, 3, 8, 7, 4 ]
 w = 10
 
-# w = 50
-# val = [60, 100, 120]
-# wt = [10, 20, 30]
+w = 50
+val = [60, 100, 120]
+wt = [10, 20, 30]
 
-n, w = len(val) + 1, w + 1
-dp = [[0 for i in range(w)] for j in range(n)]
+n, w = len(val), w
+dp = [[0 for i in range(w+1)] for j in range(n+1)]
 
-for k in range(n):
-    for l in range(w):
-        if k == 0 or l ==0:
-            dp[k][l] = 0
-
-for i in range(n):
-    print(dp[i])
-# print(n,w)
+print(n,w)
 print(ksdp(dp, wt, val, n, w))
-
-
-# not accurate solutions
